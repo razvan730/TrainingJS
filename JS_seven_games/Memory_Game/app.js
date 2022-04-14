e169075930cf38485f816e50cb91e7e758ad4c86
Razvan Savin <36405871+razvan730@users.noreply.github.com>
2022-04-14 20:21:10 +0300

    update 02

--- JS_seven_games/Memory_Game/app.js
+++ JS_seven_games/Memory_Game/app.js
@@ -1,7 +1,59 @@
-console.log("JavaScript is ready to run");
+
 const CardArray = [
     {
         name: 'fries',
-        img: ''
+        img: 'images/fries.png'
+    },
+    {
+        name: 'cheeseburger',
+        img: 'images/cheesburger.png'
+    },
+    {
+        name: 'hotdog',
+        img: 'images/hotdog.png'
+    },
+    {
+        name: 'ice-cream',
+        img: 'images/ice-cream.png'
+    },
+    {
+        name: 'milkshake',
+        img: 'images/milkshake.png'
+    }, 
+    {
+        name: 'pizza',
+        img: 'images/pizza.png'
+    },
+    {
+        name: 'fries',
+        img: 'images/fries.png'
+    },
+    {
+        name: 'cheeseburger',
+        img: 'images/cheesburger.png'
+    },
+    {
+        name: 'hotdog',
+        img: 'images/hotdog.png'
+    },
+    {
+        name: 'ice-cream',
+        img: 'images/ice-cream.png'
+    },
+    {
+        name: 'milkshake',
+        img: 'images/milkshake.png'
+    }, 
+    {
+        name: 'pizza',
+        img: 'images/pizza.png'
     }
 ]
+
+CardArray.sort(() => 0.5 - Math.random());
+//console.log(CardArray);
+
+const gridDisplay = document.querySelector('#grid')
+console.log(gridDisplay); // sa vad daca am luat id-ul grid cu querySelector
+
+
