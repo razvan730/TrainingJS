95e39c124eca543baffccc8962fa0a28b8e828b2
Razvan Savin <razvan.i.savin@gmail.com>
2022-04-23 14:07:57 +0300

    70

--- first-app/Exceptions Throw Catch Finally.js
+++ first-app/Exceptions Throw Catch Finally.js
@@ -1,10 +1,42 @@
 //
 {
+
+    // try {
+    //     dosentexist;
+
+    // } catch {
+    //     console.log("Error");
+        
+    // } finally {
+    //     console.log('test');
+    // }
+/////////////////////////////
+    // try{
+    //     dosentexist;
+    // }catch(e){
+    //     console.log(e);
+    // }finally{
+    //     console.log('test');
+    // }
+/////////////////////////////
+
+
+    // function doSomething(){
+    //     throw {error: "this is broke", code: 500};
+    // }
     
+    // try{
+    //     doSomething();
+    // }catch(e){
+    //     console.log(e);
+    //     console.log("Error");
+    // }finally{
+    //     console.log('test');
+    // }
 
+/////////////////////////////
 
-
-
+    throw new Error("this is broke"); // this is a new error , JavaScript MDN - gasesc mai multe acolo
 
 
 
