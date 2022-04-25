25e64fa36a0786c94c4041863e1b17d0bde22b7d
Razvan Savin <razvan.i.savin@gmail.com>
2022-04-25 16:44:57 +0300

    Connect Four
    prima parte

--- JS_seven_games/Connect Four/script.js
+++ JS_seven_games/Connect Four/script.js
@@ -1,1 +1,46 @@
-console.log("JavaScript is ready to run");
+document.addEventListener('DOMContentLoaded', () => {
+    const squares = document.querySelectorAll('.grid div')
+    const result = document.querySelector('#result')
+    const displayCurrentPlayer = document.querySelector('#current-player')
+    let currentPlayer = 1
+
+    for (let i = 0; i < squares.length; i++) {
+        squares[i].onclick = () => {
+          //if the square below your current square is taken, you can go on top of it
+          if (squares[i + 7].classList.contains('taken') &&!squares[i].classList.contains('taken')) {
+            if (currentPlayer == 1) {
+              squares[i].classList.add('taken')
+              squares[i].classList.add('player-one')
+              currentPlayer = 2
+              displayCurrentPlayer.innerHTML = currentPlayer
+            } else if (currentPlayer == 2){
+              squares[i].classList.add('taken')
+              squares[i].classList.add('player-two')
+              currentPlayer = 1
+              displayCurrentPlayer.innerHTML = currentPlayer        
+            } 
+          } else alert('cant go here')
+          checkBoard()
+        }
+      }
+
+
+      /// pauza, de verificat de ce nu imi apar playerii....
+
+
+
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
