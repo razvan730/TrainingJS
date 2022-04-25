97b064f545b2d57391244561d4aab7685d8306c2
Razvan Savin <razvan.i.savin@gmail.com>
2022-04-25 12:52:51 +0300

    frogger update

--- JS_seven_games/Frogger/game_app.js
+++ JS_seven_games/Frogger/game_app.js
@@ -1,75 +1,150 @@
 
 
-// // de aici... 
-// function moveFrog(e){
-     //console.log(e);
-//     switch(e.key){
-//         case 'ArrowLeft':
-//             console.log('move left');
-//             break;
-//         case 'ArrowRight':
-//             console.log('move right');
-//             break;
-//         case 'ArrowUp':
-//             console.log('move up');
-//             break;
-//         case 'ArrowDown':
-//             console.log('move down');
-//             break;
-//     }
-
-
-//     squares[currentIndex].classList.add('frog'); // trebuie sa apas o cheie ca sa imi apara... 
-// }
-// document.addEventListener('keyup', moveFrog) 
-// // ...pana aici -> este in forma simplificata, de salvat si invatat!
-
-
-
 const timeLeftDisplay = document.querySelector('#time-left')
 const resultDisplay = document.querySelector('#result')
 const startPauseButton = document.querySelector('#start-pause-button')
 const squares = document.querySelectorAll('.grid div')
+const logsLeft = document.querySelectorAll('.log-left')
+const logsRight = document.querySelectorAll('.log-right')
+const carsLeft = document.querySelectorAll('.car-left')
+const carsRight = document.querySelectorAll('.car-right')
+
+
 //console.log(squares);
 let currentIndex = 76;
-const width = 9; // 9 casute am toata latimea
+const width = 9; // 9 casute am toata latimea, ca sa ii gasesc pozitia
 
     
 
 
 function moveFrog(e){
-
-
-
+    squares[currentIndex].classList.remove('frog');
 
     switch(e.key){
         case 'ArrowLeft':
-            console.log('move left');
+            //console.log('move left');
             //currentIndex = currentIndex - 1;
             //sau
-            currentIndex -= 1;
+            if (currentIndex % width !== 0) currentIndex -= 1;
             break;
         case 'ArrowRight':
-            console.log('move right');
-            currentIndex += 1;
+            //console.log('move right');
+            if (currentIndex % width < width - 1) currentIndex += 1;  // daca sunt putin atent la ce am scris, nu e greu 
+            // curentIndex-ul e calculat tot timpul (referinta o sa am punctul A curentIndex, si B width), iar pe orizontala adaug 1 doar 
+            //daca mai e spatiu  (width < width - 1)
+            // daca uit, min 03:00:00 ( https://www.youtube.com/watch?v=ec8vSKJuZTk )
             break;
         case 'ArrowUp':
-            console.log('move up');
-            currentIndex -= width;
+            //console.log('move up');
+            if (currentIndex - width >= 0) currentIndex -= width; // e bine sa fac un patrat si sa calculez, pot dar trebuie repetat ca sa imi intru in mana, e matematica simpla
+                                                                  // daca ma agit aiurea si nu fac in pasi marunt asta, o sa-mi dau palme (reminder :)) )
             break;
         case 'ArrowDown':
-            console.log('move down');
-            currentIndex += width;
+            //console.log('move down');
+            if (currentIndex + width < width * width) currentIndex += width;
             break;
     }
 
-
-    squares[currentIndex].classList.add('frog'); // trebuie sa apas o cheie ca sa imi apara... 
+    squares[currentIndex].classList.add('frog'); 
 }
+
 document.addEventListener('keyup', moveFrog)
 
 
 
+function autoMoveElements(){
+    logsLeft.forEach(logLeft => moveLogLeft(logLeft));
+    logsRight.forEach(logRight => moveLogRight(logRight));
+    carsLeft.forEach(carLeft => moveCarLeft(carLeft));
+    carsRight.forEach(carRight => moveCarRight(carRight));
+}
+
+function moveLogLeft(logLeft){
+    switch(true){
+        case logLeft.classList.contains('l1'):
+            logLeft.classList.remove('l1')
+            logLeft.classList.add('l2')
+            break;
+        case logLeft.classList.contains('l2'):
+            logLeft.classList.remove('l2') 
+            logLeft.classList.add('l3')
+            break;
+        case logLeft.classList.contains('l3'):
+            logLeft.classList.remove('l3')
+            logLeft.classList.add('l4')
+            break;
+        case logLeft.classList.contains('l4'):
+            logLeft.classList.remove('l4')
+            logLeft.classList.add('l5')
+            break;
+        case logLeft.classList.contains('l5'):
+            logLeft.classList.remove('l5')
+            logLeft.classList.add('l1')
+            break;
+    }
+}
+
+function moveLogRight(logRight){
+    switch(true){
+        case logRight.classList.contains('l1'):
+            logRight.classList.remove('l1')
+            logRight.classList.add('l5')
+            break;
+        case logRight.classList.contains('l2'):
+            logRight.classList.remove('l2') 
+            logRight.classList.add('l1')
+            break;
+        case logRight.classList.contains('l3'):
+            logRight.classList.remove('l3')
+            logRight.classList.add('l2')
+            break;
+        case logRight.classList.contains('l4'):
+            logRight.classList.remove('l4')
+            logRight.classList.add('l3')
+            break;
+        case logRight.classList.contains('l5'):
+            logRight.classList.remove('l5')
+            logRight.classList.add('l4')
+            break;
+    }
+}
+
+
+function moveCarLeft(carLeft) {
+    switch(true) {
+        case carLeft.classList.contains('c1') :
+            carLeft.classList.remove('c1')
+            carLeft.classList.add('c2')
+            break;
+        case carLeft.classList.contains('c2') :
+            carLeft.classList.remove('c2')
+            carLeft.classList.add('c3')
+            break;
+        case carLeft.classList.contains('c3') :
+            carLeft.classList.remove('c3')
+            carLeft.classList.add('c1')
+            break;
+    }
+}
+
+function moveCarRight(carRight) {
+    switch(true) {
+        case carRight.classList.contains('c1') :
+            carRight.classList.remove('c1')
+            carRight.classList.add('c3')
+            break;
+        case carRight.classList.contains('c2') :
+            carRight.classList.remove('c2')
+            carRight.classList.add('c1')
+            break;
+        case carRight.classList.contains('c3') :
+            carRight.classList.remove('c3')
+            carRight.classList.add('c2')
+            break;
+    }
+}
+
+setInterval(autoMoveElements, 1000);
 
 
 
